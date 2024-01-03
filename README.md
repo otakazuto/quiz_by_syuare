644c9e6a5f4026c40bf3495305566da1830bcffd
kazuto <116930100+otakazuto@users.noreply.github.com>
2024-01-03 17:31:10 +0900

    問題投稿機能の追加 (#14)

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 * tailwindcssのbuild方法... docker-compose exec web rails tailwindcss:build
 
-* アセットコンパイルのコマンド docker-compose exec web bundle exec rails assets:precompile
+* アセットコンパイルのコマンド... docker-compose exec web bundle exec rails assets:precompile
+
+* tailwwindcssの変更を反映させる手順(feature/add_qustion_submissionブランチ時点)
+    
+    tailwindcssの変更を保存
+            ↓
+    docker-compose exec web bundle exec rails assets:precompile
+            ↓
+    dockerコンテナの再起動
 
 # quiz_by_syuare
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,10 +10,56 @@ class QuestionsController < ApplicationController
 
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    begin
+      ActiveRecord::Base.transaction do
+        @question = Question.create!(question_params)
+        answer = @question.create_answer(answer_params)
+        raise if answer[:explanation].blank?
+
+        choice_create_count = 0
+        true_count = 0
+        4.times do
+          choice = @question.choices.create(choice_params(choice_create_count))
+          raise if choice[:choice_text].blank?
+
+          if choice[:is_correct] == true
+            true_count += 1
+          end
+          choice_create_count += 1
+        end
+        raise if true_count != 1
+
+        flash[:notice] = "送信が完了しました。ご協力ありがとうございます。"
+        redirect_to root_path
+      end
+    rescue
+      flash[:alert] = "送信に失敗しました。()内の規定したがって作成してください。"
+      redirect_to new_question_path
+    end
+  end
+
   private
 
   def record_not_found
     flash[:error] = "レコードが見つかりませんでした。"
     redirect_to root_path
   end
+
+  def question_params
+    params.require(:question).permit(:content)
+  end
+
+  def choice_params(index)
+    params.require(:question).require(:choices).require(index.to_s).permit(:choice_text,:is_correct)
+  end
+
+  def answer_params
+    params.require(:question).require(:answer).permit(:explanation)
+  end
+
 end
